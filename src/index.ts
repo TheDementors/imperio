8744d283bd461b81684a516842cd47382a6e6120
Abhishek Chattterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2021-03-18 23:44:54 +0530

    added the constructor for the Imperio class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello World")
+class Imperio {
+  private array: Array<any>;
+  private dim: Number[];
+
+  public get shape() {
+    return this.dim;
+  }
+
+  public get(){
+      return this.array;
+  }
+
+  private static createArray(dim: Number[], defaultValue: any): Array<any> {
+    const buildArr = (dim: Number[], index: any = 0) => {
+      const arr: any = dim[index] ? new Array(dim[index]).fill(0) : 0;
+
+      for (let i = 0; i < dim[index]; i++) {
+        arr[i] = buildArr(dim, index + 1);
+      }
+
+      return arr;
+    };
+
+    return buildArr(dim);
+  }
+
+  constructor(dim: Number[], defaultValue: any) {
+    this.array = Imperio.createArray(dim, defaultValue);
+    this.dim = dim;
+  }
+}
